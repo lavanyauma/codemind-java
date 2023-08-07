dc25a84d6ba849800377d97057e218e42099fbf7
Doddalavanyaumabhavani <119916455+lavanyauma@users.noreply.github.com>
2023-08-07 14:13:48 +0530

    import java.util.Scanner; public class cmp{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int r;         double a;         r=sc.nextInt();         a=3.14*r*r;         System.out.printf("%.2f",a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class cmp{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int r;
+        double a;
+        r=sc.nextInt();
+        a=3.14*r*r;
+        System.out.printf("%.2f",a);
+    }
+}    
