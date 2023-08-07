317d0bc973a6a12d301a42bb497fba1654f63dcc
Doddalavanyaumabhavani <119916455+lavanyauma@users.noreply.github.com>
2023-08-07 14:22:48 +0530

    import java.util.Scanner; public class cmp{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a,b;         double c;         a=sc.nextInt();         b=sc.nextInt();         c=(a+b)/2.0;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class cmp{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        double c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=(a+b)/2.0;
+        System.out.printf("%.4f",c);
+    }
+}        
