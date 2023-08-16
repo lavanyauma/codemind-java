1aa2fa725ede12e9d0c4c6ad7f28c06eb1392fb1
Doddalavanyaumabhavani <119916455+lavanyauma@users.noreply.github.com>
2023-08-16 09:51:24 +0530

    import java.util.Scanner; public class cmp{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(n%2==0)         {             System.out.printf("Even");         }         else         {            System.out.printf("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class cmp{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.printf("Even");
+        }
+        else
+        {
+           System.out.printf("Odd"); 
+        }
+    }
+}
